2efb88ab91c2bf7de2f4d28cb121cf8e1b9cb623
hossein <hossein.dorosti@gmail.com>
2023-05-23 13:39:38 -0700

    fetch data using api and console log it

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const findBtn = document.getElementById('findBtn');
 findBtn.addEventListener('click', generateList);
+const apiID='tt3896198'
+const apiKey='9f1dc6dd'
 
 function generateList()
 {
     let txtSearch = document.getElementById('search-txt').value;
-    console.log(txtSearch)
-
+    fetch(`http://www.omdbapi.com/?i=${apiID}&apikey=${apiKey}&t=${txtSearch}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let html = "";
+        
+    });
 }
