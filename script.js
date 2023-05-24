3b1bd0f95ec2a424229e9d8bd715c3638b769462
hossein <hossein.dorosti@gmail.com>
2023-05-23 23:34:51 -0700

    show results

--- script.js
+++ script.js
@@ -2,15 +2,42 @@ const findBtn = document.getElementById('findBtn');
 findBtn.addEventListener('click', generateList);
 const apiID='tt3896198'
 const apiKey='9f1dc6dd'
+const movieList = document.getElementById('movie');
 
 function generateList()
 {
     let txtSearch = document.getElementById('search-txt').value;
-    fetch(`http://www.omdbapi.com/?i=${apiID}&apikey=${apiKey}&t=${txtSearch}`)
+    const url=`http://www.omdbapi.com/?i=${apiID}&apikey=${apiKey}&s=${txtSearch}`
+    console.log(url)
+    fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(data)
         let html = "";
+        // create a div in index.html and generate html code for it and show the results
         
+       if(data.Search){
+        html=`<h3>${data.totalResults} found</h3>`
+            data.Search.forEach(search => {
+                html += `
+                    <div class = "movie-item" data-id = "${search.imdbID}">
+
+                        <div class = "movie-img">
+                            <img src = "${search.Poster}" alt = ${search.Title}">
+                        </div>
+                        <div class = "Movie-Title">
+                            <h3>${search.Title}</h3>
+                            <a href = "#" class = "movieBtn">Show Details</a>
+                        </div>
+                    </div>
+                `;
+            });
+            movieList.classList.remove('NoResult');
+        } else{
+            html = "No Result!";
+            movieList.classList.add('NoResult');
+        }
+
+        movieList.innerHTML = html; 
     });
 }
